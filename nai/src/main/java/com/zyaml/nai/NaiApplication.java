6efc1136046c8cb1c47ee7ec116a1fa2872f144e
zouzhihao <1035281205@qq.com>
2020-02-12 22:19:56 +0800

    项目初始化

---
+++ nai/src/main/java/com/zyaml/nai/NaiApplication.java
@@ -0,0 +1,13 @@
+package com.zyaml.nai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaiApplication.class, args);
+    }
+
+}
---
+++ nai/src/test/java/com/zyaml/nai/NaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyaml.nai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
